fd1a55109566620550250139a5cde5d601e0fd35
Bui Anh Khoa <buianhkhoa91@gmail.com>
2015-12-21 00:06:59 +0700

    Update ChongCopy_AKgroup.js

--- ChongCopy_AKgroup.js
+++ ChongCopy_AKgroup.js
@@ -2,7 +2,13 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>Xem các bài viết hữu ích khác tại: http://auto-akgroup.com/";
+    var pagelink = "<br/><br/> Auto-AKgroup chuyên mua bán ô tô cũ tại TP.HCM
+    Tại sao bạn nên Mua Bán Ô Tô Cũ tại Auto-AKgroup:
+    - Auto-AKgroup cung cấp đa dạng các dòng xe
+    - Auto-AKgroup đảm bảo giá tốt nhất và cạnh tranh nhất thị trường Việt Nam
+    - Auto-AKgroup có nhiều chính sách cho vay ưu đãi hấp dẫn nhất
+    - Auto-AKgroup cung cấp dịch vụ bảo trì & bảo hành xe dài hạn
+    Xem các bài viết hữu ích khác tại: http://auto-akgroup.com/";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
